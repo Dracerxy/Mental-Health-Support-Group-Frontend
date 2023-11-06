3c016ec9cd2544d9b53eb75d28582274efd147fe
Sri Ganesan M <sriganesan06@gmail.com>
2023-11-06 21:00:22 +0530

    Authentication api integration

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+import {Link} from "react-router-dom";
+import { useNavigate } from 'react-router-dom'; 
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:4000/app/login', {
+        email,
+        password,
+      });
+
+      // Handle successful login here (e.g., store JWT token, update user state)
+      // console.log('Login successful:', response.data);
+      alert('Login successful!');
+      navigate('/');
+    } catch (error) {
+      // Handle login error (e.g., display error message)
+      // console.error('Login failed:', error);
+      alert('Login failed!', error);
+    }
+  }
   return (
     <div>
       <div>
@@ -15,12 +40,13 @@ const Login = () => {
 
                         <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Log In</p>
 
-                        <form className="mx-1 mx-md-4">
+                        <form className="mx-1 mx-md-4" onSubmit={handleLogin}>
 
                           <div className="d-flex flex-row align-items-center mb-4">
                             <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
                             <div className="form-outline flex-fill mb-0">
-                              <input type="email" id="form3Example3c" className="form-control" />
+                              <input type="email" id="form3Example3c" className="form-control" 
+                               value={email} onChange={(e) => setEmail(e.target.value)}/>
                               <label className="form-label" for="form3Example3c">Your Email</label>
                             </div>
                           </div>
@@ -28,14 +54,15 @@ const Login = () => {
                           <div className="d-flex flex-row align-items-center mb-4">
                             <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
                             <div className="form-outline flex-fill mb-0">
-                              <input type="password" id="form3Example4c" className="form-control" />
+                              <input type="password" id="form3Example4c" className="form-control" 
+                              value={password} onChange={(e) => setPassword(e.target.value)}/>
                               <label className="form-label" for="form3Example4c">Password</label>
                             </div>
                           </div>
 
                           <div className="action d-flex align-items-center mx-1 mb-3 mb-lg-4" style={{gap: '33%'}}>
                             <p className=''><a href="#" class="link-dark link-offset-2 link-underline-opacity-0">Forgot your password</a></p>
-                            <button type="button" className="btn btn-primary btn-lg d-flex justify-content-end">Submit</button>
+                            <button type="submit" className="btn btn-primary btn-lg d-flex justify-content-end">Submit</button>
                           </div>
 
                         </form>
--- src/Component/Signup.js
+++ src/Component/Signup.js
@@ -1,6 +1,54 @@
 import React from 'react'
-
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom'; 
 const Signup = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [repeatPassword, setRepeatPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:4000/app/signup', {
+        name,
+        email,
+        password,
+      });
+
+      // Handle successful registration here (e.g., store JWT token, update user state)
+    //   console.log('Registration successful:', response.data);
+      alert('Registration successful:', response.data);
+      navigate('/');
+    } catch (error) {
+      // Handle registration error (e.g., display error message)
+    //   console.error('Registration failed:', error);
+        alert('Registration failed:', error);
+        }
+    }
+    const handlenameChange = (e) => {
+        const newname = e.target.value;
+        setName(newname);
+    };
+    const handleemailChange = (e) => {
+        const newemail = e.target.value;
+        setEmail(newemail);
+    };
+    const handlePasswordChange = (e) => {
+        const newPassword = e.target.value;
+        setPassword(newPassword);
+    };
+
+    const handleRepeatPasswordChange = (e) => {
+        const newRepeatPassword = e.target.value;
+        setRepeatPassword(newRepeatPassword);
+    };
+
+    const passwordsMatch = password === repeatPassword;
+
+
     return (
         <div>
             <section className="vh-100" >
@@ -14,12 +62,13 @@ const Signup = () => {
 
                                             <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
 
-                                            <form className="mx-1 mx-md-4">
+                                            <form onSubmit={handleRegister} className="mx-1 mx-md-4">
 
                                                 <div className="d-flex flex-row align-items-center mb-4">
                                                     <i className="fas fa-user fa-lg me-3 fa-fw"></i>
                                                     <div className="form-outline flex-fill mb-0">
-                                                        <input type="text" id="form3Example1c" className="form-control" />
+                                                        <input value={name} type="text" id="form3Example1c" className="form-control"
+                                                         onChange={handlenameChange}/> 
                                                         <label className="form-label" for="form3Example1c">Your Name</label>
                                                     </div>
                                                 </div>
@@ -27,26 +76,35 @@ const Signup = () => {
                                                 <div className="d-flex flex-row align-items-center mb-4">
                                                     <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
                                                     <div className="form-outline flex-fill mb-0">
-                                                        <input type="email" id="form3Example3c" className="form-control" />
+                                                        <input value={email} type="email" id="form3Example3c" className="form-control"
+                                                         onChange={handleemailChange}/> 
                                                         <label className="form-label" for="form3Example3c">Your Email</label>
                                                     </div>
                                                 </div>
 
-                                                <div className="d-flex flex-row align-items-center mb-4">
+                                                <div className={`d-flex flex-row align-items-center mb-4 ${passwordsMatch ? '' : 'has-warning'}`}>
                                                     <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
                                                     <div className="form-outline flex-fill mb-0">
-                                                        <input type="password" id="form3Example4c" className="form-control" />
+                                                        <input value={password} type="password" id="form3Example4c"  className={`form-control ${passwordsMatch ? '' : 'is-invalid'}`}
+                                                         onChange={handlePasswordChange}/>
                                                         <label className="form-label" for="form3Example4c">Password</label>
                                                     </div>
                                                 </div>
 
-                                                <div className="d-flex flex-row align-items-center mb-4">
+                                                <div className={`d-flex flex-row align-items-center mb-4 ${passwordsMatch ? '' : 'has-warning'}`}>
                                                     <i className="fas fa-key fa-lg me-3 fa-fw"></i>
                                                     <div className="form-outline flex-fill mb-0">
-                                                        <input type="password" id="form3Example4cd" className="form-control" />
+                                                        <input value={repeatPassword} type="password" id="form3Example4cd" 
+                                                       className={`form-control ${passwordsMatch ? '' : 'is-invalid'}`}
+                                                       onChange={handleRepeatPasswordChange} />
                                                         <label className="form-label" for="form3Example4cd">Repeat your password</label>
                                                     </div>
                                                 </div>
+                                                {!passwordsMatch && (
+                                                        <div className="alert alert-danger" role="alert">
+                                                        Passwords do not match.
+                                                        </div>
+                                                    )}
 
                                                 <div className="form-check d-flex justify-content-center mb-5">
                                                     <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
@@ -56,7 +114,7 @@ const Signup = () => {
                                                 </div>
 
                                                 <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                                    <button type="button" className="btn btn-primary btn-lg">Register</button>
+                                                    <button type="submit" className="btn btn-primary btn-lg">Register</button>
                                                 </div>
 
                                             </form>
