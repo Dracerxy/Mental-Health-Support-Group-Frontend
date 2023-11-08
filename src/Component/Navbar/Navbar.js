f59bc7d12a17316b89eb1ac4ef9636cc2c10a502
Sri Ganesan M <100109259+Dracerxy@users.noreply.github.com>
2023-11-08 13:56:19 +0530

    minor fix

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -77,9 +77,9 @@ const Navbar = () => {
         }
     }
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">AppName</a>
+                <a className="nav-brand" href="/">AppName</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
