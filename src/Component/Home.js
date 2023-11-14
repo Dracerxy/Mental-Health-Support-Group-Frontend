fdaa999ff906ec8915e0cfc5161fb04874d3ed3e
Ashish-singh-1112 <as8239613@gmail.com>
2023-11-14 12:44:13 +0530

    Home page

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import './Home.css'
+import Chat from './Chat/Chat'
+import LandingPage from './LandingPage/LandingPage'
 
 const Home = () => {
+
   return (
-    <div>
-      <h1>I am Home</h1>
+    <div className="container-fluid">
+      <div class="row">
+        <div className="col-9 col-home">
+          <LandingPage />
+        </div>
+        <div className="col col-chat">
+          <Chat />
+        </div>
+      </div>
     </div>
   )
 }
