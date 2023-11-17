e090399005ee14d72d02eef227e29240a09c2a30
sarthakgit99 <144561984+sarthakgit99@users.noreply.github.com>
2023-11-17 11:17:19 +0530

    Done resources page

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
 const Home = () => {
   return (
     <div>
-      <h1>I am Home</h1>
+      {/* <h1>I am Home</h1> */}
+      <img src="https://images.creativemarket.com/0.1.0/ps/15888712/2340/1560/m1/fpnw/wm0/mental-health-awareness-(isolated)-.jpg?1&s=91635b1a7c58cce53a0706a39dd771ed" width="1200px" height="800px"/>
     </div>
   )
 }
--- src/Component/Resources/Resources.js
+++ src/Component/Resources/Resources.js
@@ -1,11 +1,50 @@
-import React from 'react'
-
-const Resources = () => {
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Structure from './Structure.js';
+import Data from './Data.js'
+import './res.css'
+const Resources = (props) => {
   return (
-    <div>
-      <h1>I am Resources</h1>
+    <div className="resources-page">
+      <h1>Mental Health Resources</h1>
+      <section>
+        <h2>Blogs</h2>
+        <div className="resource-list">
+          {Data
+            .filter((Resources) => Resources.type === "blog")
+            .map((blog, index) => (
+              <div className="resource-item" key={index}>
+                <div className="card">
+                  <h3>{blog.title}</h3>
+                  <img src={blog.image} alt="Resource Image" className="card-image" />
+                  <p>{blog.description}</p>
+                  <a href={blog.link} target="_blank" rel="noopener noreferrer">
+                    Read More
+                  </a>
+                </div>
+              </div>
+            ))}
+        </div>
+      </section>
+      <section>
+        <h2 id='heading'>Videos</h2>
+        <div className="resource-list">
+          {Data
+            .filter((Resources) => Resources.type === "video")
+            .map((video, index) => (
+              <div className="resource-item" key={index}>
+                <div className="card">
+                  <h3>{video.title}</h3>
+                  <img src={video.image} alt="Resource Image" className="card-image"/>
+                  <p>{video.description}</p>
+                  <a href={video.link} target="_blank" rel="noopener noreferrer">
+                    Watch Now!
+                  </a></div>
+              </div>
+            ))}
+        </div>
+      </section>
     </div>
-  )
-}
-
-export default Resources
+  );
+};
+export default Resources;
