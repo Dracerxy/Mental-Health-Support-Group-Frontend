7d258366d750b53b592fed677f804c63e62f9aaf
Ashish-singh-1112 <as8239613@gmail.com>
2023-11-18 12:16:15 +0530

    Resources ui part

--- src/Component/Resources/Resources.js
+++ src/Component/Resources/Resources.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Structure from './Structure.js';
 import Data from './Data.js'
 import './res.css'
 const Resources = (props) => {
   return (
     <div className="resources-page">
       <h1>Mental Health Resources</h1>
-      <section>
+      <section className='blogs'>
         <h2>Blogs</h2>
         <div className="resource-list">
           {Data
             .filter((Resources) => Resources.type === "blog")
             .map((blog, index) => (
               <div className="resource-item" key={index}>
-                <div className="card">
+                <div className="card res-card">
                   <h3>{blog.title}</h3>
                   <img src={blog.image} alt="Resource Image" className="card-image" />
                   <p>{blog.description}</p>
@@ -33,13 +31,14 @@ const Resources = (props) => {
             .filter((Resources) => Resources.type === "video")
             .map((video, index) => (
               <div className="resource-item" key={index}>
-                <div className="card">
+                <div className="card res-card">
                   <h3>{video.title}</h3>
-                  <img src={video.image} alt="Resource Image" className="card-image"/>
+                  <img src={video.image} alt="Resource Image" className="card-image" />
                   <p>{video.description}</p>
                   <a href={video.link} target="_blank" rel="noopener noreferrer">
                     Watch Now!
-                  </a></div>
+                  </a>
+                </div>
               </div>
             ))}
         </div>
